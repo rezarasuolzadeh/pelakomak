d862eeccc09aea8f8c7d9860e0cee791c7d0107b
Reza <reza.rasoulzadeh@hotmail.com>
2019-10-12 09:25:22 +0330

    commit 5 - resolve the snack bar view problems.

---
+++ app/src/main/java/ir/rezarasoulzadeh/pelakomak/snackbar/Snackbar.kt
@@ -0,0 +1,37 @@
+package ir.rezarasoulzadeh.pelakomak.snackbar
+
+import android.view.View
+import android.widget.TextView
+import android.view.LayoutInflater
+import ir.rezarasoulzadeh.pelakomak.R
+import com.google.android.material.snackbar.Snackbar
+
+class Snackbar {
+
+    fun show(message: String, duration: String, view: View, inflater: LayoutInflater) {
+
+        val snackbarView: View = inflater.inflate(R.layout.snackbar_for_message, null)
+
+        val snackbarText = snackbarView.findViewById(R.id.snackbar_message) as TextView
+
+        val snackbar: Snackbar
+
+        if (duration == "short")
+            snackbar = Snackbar.make(view, "", Snackbar.LENGTH_SHORT)
+        else
+            snackbar = Snackbar.make(view, "", Snackbar.LENGTH_LONG)
+
+        val layout = snackbar.view as Snackbar.SnackbarLayout
+
+        val textView = layout.findViewById(R.id.snackbar_text) as TextView
+        textView.visibility = View.INVISIBLE
+
+        snackbarText.text = message
+
+        layout.setPadding(0, 0, 0, 0)
+
+        layout.addView(snackbarView, 0)
+
+        snackbar.show()
+    }
+}
