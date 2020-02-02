77bc6dd47cad7a8ce776995364ae7d74e234d4ac
Reza Rasoulzadeh <reza.rasoulzadeh@hotmail.com>
2020-02-03 00:38:50 +0330

    -- complete dialog for summary -- complete dialog for help

--- app/src/main/java/ir/rezarasoulzadeh/pelakomak/view/activity/FoulActivity.kt
+++ app/src/main/java/ir/rezarasoulzadeh/pelakomak/view/activity/FoulActivity.kt
@@ -12,15 +12,25 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import ir.rezarasoulzadeh.pelakomak.R
+import ir.rezarasoulzadeh.pelakomak.model.Foul
+import ir.rezarasoulzadeh.pelakomak.service.utils.Format
 import ir.rezarasoulzadeh.pelakomak.view.adapter.FoulAdapter
 import ir.rezarasoulzadeh.pelakomak.viewmodel.FoulViewModel
 import kotlinx.android.synthetic.main.activity_foul.*
-import kotlinx.android.synthetic.main.dialog_for_save.view.*
 import kotlinx.android.synthetic.main.dialog_for_sort.view.*
+import kotlinx.android.synthetic.main.dialog_for_summary.view.*
 
-class FoulActivity : AppCompatActivity(){
+class FoulActivity : AppCompatActivity() {
 
     private lateinit var foulViewModel: FoulViewModel
+
+    private lateinit var plaqueFirstPart: String
+    private lateinit var plaqueSecondPart: String
+    private lateinit var plaqueThirdPart: String
+    private lateinit var plaqueFourthPart: String
+
+    private lateinit var foulCount: String
+    private lateinit var foulPrice: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +38,12 @@ class FoulActivity : AppCompatActivity(){
 
         supportActionBar!!.hide()
 
+        barcodeEditText.setText(R.string.baba)
+
         foulViewModel = ViewModelProviders.of(this).get(FoulViewModel::class.java)
 
         saveButton.setOnClickListener {
-            barcodeEditText.setText(R.string.barcode)
-
-            val saveView = LayoutInflater.from(this).inflate(R.layout.dialog_for_save, null)
+            val saveView = LayoutInflater.from(this).inflate(R.layout.dialog_for_summary, null)
 
             val saveViewBuilder = this.let { it1 -> AlertDialog.Builder(it1).setView(saveView) }
 
@@ -41,17 +51,13 @@ class FoulActivity : AppCompatActivity(){
 
             saveAlertDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-            saveView.pdfButton.setOnClickListener {
-                Handler().postDelayed({
-                    saveAlertDialog.dismiss()
-                }, 500)
-            }
+            saveView.plaqueFirstTextView.text = plaqueFirstPart
+            saveView.plaqueSecondTextView.text = plaqueSecondPart
+            saveView.plaqueThirdTextView.text = plaqueThirdPart
+            saveView.plaqueFourthTextView.text = plaqueFourthPart
+            saveView.foulCountTextView.text = foulCount
+            saveView.foulPriceTextView.text = foulPrice
 
-            saveView.txtButton.setOnClickListener {
-                Handler().postDelayed({
-                    saveAlertDialog.dismiss()
-                }, 500)
-            }
         }
 
         getFoulsButton.setOnClickListener {
@@ -64,6 +70,13 @@ class FoulActivity : AppCompatActivity(){
                 foulRecyclerView.adapter = adapter
                 foulRecyclerView.visibility = View.VISIBLE
                 emptyView.visibility = View.GONE
+                val section = plaqueSection(it[0].plaque)
+                plaqueFirstPart = section[0]
+                plaqueSecondPart = section[1]
+                plaqueThirdPart = section[2]
+                plaqueFourthPart = section[3]
+                foulCount = it.size.toString()
+                foulPrice = finallPrice(it)
             })
         }
 
@@ -91,4 +104,23 @@ class FoulActivity : AppCompatActivity(){
         }
 
     }
+
+    private fun plaqueSection(plaque: String): ArrayList<String> {
+        val section = ArrayList<String>(4)
+        section.add("${plaque[4]}${plaque[5]}")
+        section.add("${plaque[3]}")
+        section.add("${plaque[0]}${plaque[1]}${plaque[2]}")
+        section.add("${plaque[6]}${plaque[7]}")
+        return section
+    }
+
+    private fun finallPrice(fouls: List<Foul>) : String {
+        val format = Format()
+        var price = 0
+        for (i in fouls.indices) {
+            price += fouls[i].amount
+        }
+        return format.priceFormat(price)
+    }
+
 }
